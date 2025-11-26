92d44b5f72f90b49799db47b5af1ca17ec842189
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-11-26 14:59:13 +1000

    add notifications infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,10 @@ resource "aws_s3_bucket" "stock_data_bucket" {
   bucket = "snowflake-platform-stock-data"
 }
 
+resource "aws_s3_bucket" "stock_data_notifications_bucket" {
+  bucket = "snowflake-platform-stock-data-notifications"
+}
+
 # Lambda
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -21,6 +25,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# Lambda 1: Stock Data
 data "aws_iam_policy_document" "sns_publish" {
   statement {
     effect = "Allow"
@@ -35,18 +40,18 @@ data "aws_iam_policy_document" "sns_publish" {
   }
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name               = "snowflake-platform-lambda-execution-role"
+resource "aws_iam_role" "stock_data_lambda_role" {
+  name               = "snowflake-platform-stock-data-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_execution_role.name
+resource "aws_iam_role_policy_attachment" "stock_data_basic_execution" {
+  role       = aws_iam_role.stock_data_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy" "lambda_sns_publish" {
-  role = aws_iam_role.lambda_execution_role.name    
+  role = aws_iam_role.stock_data_lambda_role.name    
   policy = data.aws_iam_policy_document.sns_publish.json
 }
 
@@ -59,7 +64,7 @@ data "archive_file" "stock_data" {
 resource "aws_lambda_function" "stock_data" {
   filename         = data.archive_file.stock_data.output_path
   function_name    = "stock-data"
-  role             = aws_iam_role.lambda_execution_role.arn
+  role             = aws_iam_role.stock_data_lambda_role.arn
   handler          = "stock_data.handler"
   source_code_hash = data.archive_file.stock_data.output_base64sha256
   runtime = "python3.10"
@@ -92,14 +97,94 @@ resource "aws_lambda_permission" "eventbridge_invoke_stock_data" {
     source_arn = aws_cloudwatch_event_rule.every_minute.arn
 }
 
+# Lambda 2: Stock Data Notifications
+data "aws_iam_policy_document" "sqs_handle_messages" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+
+    resources = [
+      aws_sqs_queue.stock_data_notifications.arn
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "s3_put" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.stock_data_notifications_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "stock_data_notifications_lambda_role" {
+  name               = "snowflake-platform-stock-data-notifications-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "stock_data_notifications_basic_execution" {
+  role       = aws_iam_role.stock_data_notifications_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "lambda_sqs_handle_messages" {
+  role = aws_iam_role.stock_data_notifications_lambda_role.name 
+  policy = data.aws_iam_policy_document.sqs_handle_messages.json
+}
+
+resource "aws_iam_role_policy" "lambda_s3_put" {
+  role = aws_iam_role.stock_data_notifications_lambda_role.name 
+  policy = data.aws_iam_policy_document.s3_put.json
+}
+
+data "archive_file" "stock_data_notifications" {
+  type        = "zip"
+  source_file = "${path.module}/lambdas/stock_data_notifications.py"
+  output_path = "${path.module}/lambdas/stock_data_notifications.py.zip"
+}
+
+resource "aws_lambda_function" "stock_data_notifications" {
+  filename         = data.archive_file.stock_data_notifications.output_path
+  function_name    = "stock-data-notifications"
+  role             = aws_iam_role.stock_data_notifications_lambda_role.arn
+  handler          = "stock_data_notifications.handler"
+  source_code_hash = data.archive_file.stock_data_notifications.output_base64sha256
+  runtime = "python3.10"
+
+    environment {
+        variables = {
+        QUEUE_NAME = aws_sqs_queue.stock_data_notifications.name
+        BUCKET_NAME = aws_s3_bucket.stock_data_notifications_bucket.bucket
+        }
+    }
+}
+
+resource "aws_lambda_event_source_mapping" "stock_data_notifications_sqs" {
+  event_source_arn = aws_sqs_queue.stock_data_notifications.arn
+  function_name    = aws_lambda_function.stock_data_notifications.arn
+  batch_size       = 10          # Number of messages per invocation
+  enabled          = true
+}
+
 # SNS
 resource "aws_sns_topic" "stock_data" {
   name = "stock-data-topic"
 }
 
 # SQS
-resource "aws_sqs_queue" "stock_data_errors" {
-  name = "stock-data-errors-queue"
+resource "aws_sqs_queue" "stock_data_notifications" {
+  name = "stock-data-notifications-queue"
 }
 
 data "aws_iam_policy_document" "sns_send_message_to_sqs" {
@@ -116,7 +201,7 @@ data "aws_iam_policy_document" "sns_send_message_to_sqs" {
     ]
 
     resources = [
-      aws_sqs_queue.stock_data_errors.arn
+      aws_sqs_queue.stock_data_notifications.arn
     ]
 
     condition {
@@ -128,12 +213,12 @@ data "aws_iam_policy_document" "sns_send_message_to_sqs" {
 }
 
 resource "aws_sqs_queue_policy" "sns_send_message_to_sqs" {
-  queue_url = aws_sqs_queue.stock_data_errors.id
+  queue_url = aws_sqs_queue.stock_data_notifications.id
   policy    = data.aws_iam_policy_document.sns_send_message_to_sqs.json
 }
 
-resource "aws_sns_topic_subscription" "stock_data_errors" {
+resource "aws_sns_topic_subscription" "stock_data_notifications" {
   topic_arn = aws_sns_topic.stock_data.arn
-  endpoint  = aws_sqs_queue.stock_data_errors.arn
+  endpoint  = aws_sqs_queue.stock_data_notifications.arn
   protocol  = "sqs"
 }
