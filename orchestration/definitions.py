ad9bce73f5be700945fcddd0e54b001dc8da4028
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-31 21:47:50 +1000

    update dagster fic dagster/dagster-dbt versions

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,6 +1,6 @@
 from dagster import Definitions
 from orchestration.assets.airbyte import get_airbyte_objects
-from orchestration.assets.dbt import dbt_warehouse, dbt_warehouse_resource
+# from orchestration.assets.dbt import dbt_warehouse, dbt_warehouse_resource
 
 
 airbyte_workspace, airbyte_assets = get_airbyte_objects()
@@ -9,6 +9,6 @@ defs = Definitions(
     assets=[*airbyte_assets, dbt_warehouse],
     resources={
         "airbyte": airbyte_workspace,
-        "dbt_warehouse_resource": dbt_warehouse_resource
+        # "dbt_warehouse_resource": dbt_warehouse_resource
         }
 )
